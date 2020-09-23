f9abdb5c6615b9485b6418db1e8b0fd42503c0c6
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-09-23 16:22:25 +0300

    added first route of the mails

--- src/routes/mail.ts
+++ src/routes/mail.ts
@@ -1,9 +1,31 @@
 import  Router from 'koa-router';
+import main from '../util/mailer';
 
 const router = new Router();
 
-router.get('/api/email/toUser/:email', async (ctx, err) => {
-    const email = ctx.params.email;
+router.get('/api/email/oplata/toUser/:email/:name/', async (ctx, err) => {
+    const email: string = ctx.params.email;
+    const name: string = ctx.params.name;
+
+    const subject: string = "Оплата додатку";
+    const text: string = `<h1>Шановний ${name}</h1>
+        <p> Через 5 днів закінчується термін
+        сплаченого періоду за додаток «Way
+        without problem»
+        Будь ласка, сплатіть за додаток, щоб
+        використовувати всі його функції
+        </p>
+        <h1>Команда «Way without problem» </h1>
+    `;
+
+    await main(text, subject, email);
+});
+
+router.post('/api/email/new-fee/toUser', async(ctx,err)=>{
+    
+});
+
+router.post('/api/email/status-fee/toUser', async(ctx,err)=>{
 
 });
 
