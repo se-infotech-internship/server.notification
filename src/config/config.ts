ea171e1dcb65ba60cfc10bea11da79e6e80305a9
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-09-23 16:21:28 +0300

    configuration file

---
+++ src/config/config.ts
@@ -0,0 +1,17 @@
+const config = {
+
+    development:{
+        port: process.env.port || 3000,
+
+    },
+
+    mailer:{
+        user: "way.without.problem@gmail.com",
+        password:"byajnt[2020"
+    },
+
+
+
+};
+
+export default config;
