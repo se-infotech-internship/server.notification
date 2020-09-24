1e0faeb38e3ab577db66ec4eb536d6547f0e88d2
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-09-24 13:01:11 +0300

    builded from src

--- dist/app.js
+++ dist/app.js
@@ -5,12 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const koa_1 = __importDefault(require("koa"));
 const koa_router_1 = __importDefault(require("koa-router"));
+const koa_bodyparser_1 = __importDefault(require("koa-bodyparser"));
 const mail_1 = __importDefault(require("./routes/mail"));
 const config_1 = __importDefault(require("./config/config"));
 const app = new koa_1.default();
 const router = new koa_router_1.default();
 const port = config_1.default.development.port;
-router.get('/api', () => { console.log('hello'); });
+app.use(koa_bodyparser_1.default());
 app.use(mail_1.default.routes());
 app.listen(port, () => {
     console.log("it works!");
--- dist/config/config.js
+++ dist/config/config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
     development: {
-        port: 3000,
+        port: process.env.port || 3000,
     },
     mailer: {
         user: "way.without.problem@gmail.com",
