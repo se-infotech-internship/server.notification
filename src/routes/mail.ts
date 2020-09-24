d4912aab74a722ce4aa9bdbd7af160a563c55cb7
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-09-24 15:58:21 +0300

    a bit changed text of messages

--- src/routes/mail.ts
+++ src/routes/mail.ts
@@ -26,9 +26,11 @@ router.post('/api/email/new-fee/toUser', async (ctx: Context, err) => {
     const { email, fee, name } = ctx.request.body;
 
     const subject: string = "Новий штраф!"
-    const text: string = `<h1>Шановний ${name}!</h1>
-        <p> Ви маєте несплачений штраф:
-        ${fee}
+    const text: string = `<h1>Шановний, ${name}!</h1>
+        <p> Ви маєте несплачений штраф: <h2> ${fee.title} </h2>
+        </p>
+        <p>
+        Ви порушили: ${fee.data}
         </p>
         <h3>Команда «Way without problem» </h3>
     `;
@@ -37,15 +39,15 @@ router.post('/api/email/new-fee/toUser', async (ctx: Context, err) => {
 });
 // changed status of fee email
 router.post('/api/email/status-fee/toUser', async (ctx: Context, err) => {
-    const { email, fee, status, name } = ctx.request.body;
+    const { email, fee, name } = ctx.request.body;
 
     const subject: string = "Статус штрафу!"
     const text: string = `<h1>Шановний, ${name}!</h1>
-        <p> Ви маєте несплачений штраф:
-        ${fee}
+        <p> Ваш штраф: 
+        "${fee.title}", скоєний : ${fee.data}
         </p>
         <p>
-        Статус Вашого сплаченого штрафу – ${status}
+        Має новий статус оплати – ${fee.status}!
         </p>
         <h3>Команда «Way without problem» </h3>
     `;
