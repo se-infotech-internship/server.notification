b7d1d0f10a861b959eac4906400ae94bc2be12ca
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-09-28 15:33:45 +0300

    added push notifications router

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,17 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 
 import MailRoutes from './routes/mail';
+import PushRoutes from './routes/pushNotifications';
 import config from './config/config';
 
 const app = new koa();
-
-const router = new Router();
 
 const port = config.development.port;
 
 app.use(bodyParser());
 
 app.use(MailRoutes.routes());
+app.use(PushRoutes.routes());
 
 
 
